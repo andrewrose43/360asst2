f4f8e770a2b9aa103da66c0c8a840c7c24d02e09
Andrew <andrewrose@uvic.ca>
2019-02-28 00:04:23 -0800

    Printing AS SPECIFIED, but repeated. Adding compiler flags to skip repetitions...

--- pc_mutex_cond_pthread.c
+++ pc_mutex_cond_pthread.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <assert.h>
+#include <string.h>
 
-int items = 0; //The buffer
-int producer_wait_count;
-int consumer_wait_count;
-
-pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 #define MAX_ITEMS 10 //Max size of buffer
 #define NUM_ITERATIONS 200 //Items to pass through program
 #define NUM_PRODUCERS 2 //Number of threads adding to buffer
 #define NUM_CONSUMERS 2 //Number of threads removing from buffer
 #define RUNS 50 //The number of times to run the threads, to flush out errors
+
+int items = 0; //The buffer
+int producer_wait_count;
+int consumer_wait_count;
+int histogram[MAX_ITEMS+1];
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 void* producer(void* v){
 	//printf("Producer thread begins!\n");
@@ -20,7 +22,8 @@ void* producer(void* v){
 		pthread_mutex_lock(&mutex1);
 		if (items < MAX_ITEMS){
 			//iterations should only count if they successfully produced
-			items++;
+			++items;
+			++histogram[items];
 		}
 		else {
 			--i;
@@ -40,6 +43,7 @@ void* consumer (void* v) {
 		if (items){
 			--items;
 			assert(0<=items && items <=MAX_ITEMS);
+			++histogram[items];
 		}
 		//iterations should only count if they successfully consumed
 		else{
@@ -64,32 +68,42 @@ int main(){
 		//printf("pc_mutex_cond_pthread run #%d\n", run);
 		
 		//Resetting key variables
-
 		producer_wait_count = 0;
 		consumer_wait_count = 0;
+		memset(histogram, 0, sizeof(histogram)); //all elements to 0
 	
-		for (int i = 0; i < NUM_PRODUCERS; i++){
+		for (int i = 0; i < NUM_PRODUCERS; ++i){
 			if ((error_number=pthread_create(&pros[i], NULL, &producer, NULL))){
 				printf("Producer thread creation failed: %d\n", error_number);
 			}
 		}
-		for (int i = 0; i < NUM_CONSUMERS; i++){
+		for (int i = 0; i < NUM_CONSUMERS; ++i){
 			if ((error_number=pthread_create(&cons[i], NULL, &consumer, NULL))){
 				printf("Consumer thread creation failed: %d\n", error_number);
 			}
 		}
 
 		//Wait until threads are done...
-		for (int i = 0; i < NUM_PRODUCERS; i++){
+		for (int i = 0; i < NUM_PRODUCERS; ++i){
 			pthread_join(pros[i], NULL);
 			//printf("Producer thread #%d is finished\n", i);
 		}
-		for (int i = 0; i < NUM_CONSUMERS; i++){
+		for (int i = 0; i < NUM_CONSUMERS; ++i){
 			pthread_join(cons[i], NULL);
 			//printf("Consumer thread #%d is finished\n", i);
 		}
 	
-		printf("Run %d complete!\nProducer wait count: %d\nConsumer wait count: %d\n\n", run, producer_wait_count, consumer_wait_count);
+		//Print the wait counters
+		printf ("producer_wait_count=%d\nconsumer_wait_count=%d\n", producer_wait_count, consumer_wait_count);
+		//Print the histogram
+		printf ("items value histogram:\n");
+		int sum=0;
+		for (int i = 0; i <= MAX_ITEMS; i++) {
+			printf ("  items=%d, %d times\n", i, histogram [i]);
+			sum += histogram [i];
+		}
+
+		printf("\n\n");
 	}
 	printf("All runs complete!\n\n");
 	exit(0);
